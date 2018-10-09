d5911338c323f5b1af99a8aa8cd41fb3c8d7caa3
hilary <hanhan0602@gmail.com>
2018-10-09 08:56:56 +0800

    1009教學案例5-獲取網絡數據

--- app/src/main/java/com/pku/weatherapp/MainActivity.java
+++ app/src/main/java/com/pku/weatherapp/MainActivity.java
@@ -1,32 +1,104 @@
 package com.pku.weatherapp;
 
+import android.content.SharedPreferences;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.pku.weatherapp.util.NetUtil;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ImageView mUpdateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
         checkNetwork();
     }
 
-    private void checkNetwork(){
-        if(NetUtil.getNetworkState(this)!=NetUtil.NETWORN_NONE){
+    private void initView() {
+
+        mUpdateBtn = findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+
+    }
+
+    private void checkNetwork() {
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
             Log.d(TAG, "checkNetwork: " + "網路OK");
-            Toast.makeText(this,"網路OK",Toast.LENGTH_SHORT).show();
-        }else {
+            Toast.makeText(this, "網路OK", Toast.LENGTH_SHORT).show();
+        } else {
             Log.d(TAG, "checkNetwork: " + "網路掛了");
-            Toast.makeText(this,"網路掛了",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "網路掛了", Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.title_update_btn) {
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code", "101010100");
+            Log.d(TAG, "onClick: cityCode " + cityCode);
+
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+                Log.d(TAG, "checkNetwork: " + "網路OK");
+                queryWeatherCode(cityCode);
+            } else {
+                Log.d(TAG, "checkNetwork: " + "網路掛了");
+                Toast.makeText(this, "網路掛了", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void queryWeatherCode(String cityCode) {
+
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d(TAG, "queryWeatherCode: address "+address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(address);
+                    connection = (HttpURLConnection)url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while((str=reader.readLine()) != null){
+                        response.append(str);
+                    }
+                    String responseStr=response.toString();
+                    Log.d(TAG, "queryWeatherCode: "+responseStr);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if(connection != null){
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
 }
